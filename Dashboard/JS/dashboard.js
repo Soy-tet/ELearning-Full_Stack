bf970af212ad93816dc768da94f6ed1b7362afc3
Easti <114486018+Soy-tet@users.noreply.github.com>
2023-02-25 21:28:59 +0700

    1.1.2

--- Dashboard/JS/dashboard.js
+++
@@ -1,9 +0,0 @@
-function home() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML =
-      xhttp.responseText;
-    }
-    xhttp.open("GET", "./PHP/home.php");
-    xhttp.send();
-  }
--- Dashboard/SQL/Function/chack_session.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    if(!isset($_SESSION['username'])){
-        header('location: ./PHP/login.php?ref='.$_SERVER["PHP_SELF"]);
-    }
-?>
